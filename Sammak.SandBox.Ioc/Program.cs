5248a2d3c90fce843d37602ec2d64d29ff6df6a8
Jeff Sammak <jeff.sammak@cardionet.com>
2019-06-09 12:46:46 -0400

    Add project files.

---
+++ Sammak.SandBox.Ioc/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using StructureMap;
+namespace Sammak.SandBox.Ioc
+{
+    /// <summary>
+    /// See http://stackoverflow.com/questions/6777671/setting-up-structure-map-in-a-c-sharp-console-application
+    /// Updated for SM 4: http://ardalis.com/using-structuremap-4-in-a-console-app
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is required to wire up your types - it should always happen when you app is starting up
+            var container = InitIoC();
+            Console.WriteLine("StructureMap Initialized.");
+
+            // this takes advantage of the WithDefaultConventions() feature of StructureMap and will result in the Message type coming back.
+            IMessage myMessage = container.GetInstance<IMessage>();
+            Console.WriteLine(myMessage.Hello("Steve"));
+
+            // this uses the config.For<> syntax in the InitIoC() method
+            var myPerson = container.GetInstance<Person>(); // getting a Person will get all dependencies specified in its constructor
+            myPerson.Name = "Steve";
+            myPerson.Age = 38;
+            Console.WriteLine("Person:");
+            Console.WriteLine(myPerson);
+
+            Console.ReadLine();
+        }
+
+        private static Container InitIoC()
+        {
+            var container = new Container(cfg =>
+            {
+                // DefaultConventions
+                cfg.Scan(x =>
+                {
+                    x.TheCallingAssembly();
+                    x.WithDefaultConventions();
+                });
+
+                // specific implementation for certain interface
+                cfg.For<IPersonFormatter>().Use<CapsPersonFormatter>();
+
+                // config of same interface the last one wins.  
+                // in this case even though the For<IPersonFormatter> 
+                // implementaion of CapsPersonFormatter was defined earlier, 
+                // the Use<SimplePersonFormatter> will win
+                cfg.For<IPersonFormatter>().Use<SimplePersonFormatter>();
+            });
+            return container;
+        }
+    }
+
+    public interface IMessage
+    {
+        string Hello(string person);
+    }
+
+    public class Message : IMessage
+    {
+        public string Hello(string person)
+        {
+            return String.Format("HELLO {0}!!!", person);
+        }
+    }
+
+    public interface IPersonFormatter
+    {
+        string Format(Person person);
+    }
+
+    public class Person
+    {
+        private readonly IPersonFormatter _personFormatter;
+
+        public Person(IPersonFormatter personFormatter)
+        {
+            _personFormatter = personFormatter;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return _personFormatter.Format(this);
+        }
+    }
+
+    public class SimplePersonFormatter : IPersonFormatter
+    {
+        public string Format(Person person)
+        {
+            return $"{person.Name}, {person.Age} years old.";
+        }
+    }
+
+    public class CapsPersonFormatter : IPersonFormatter
+    {
+        public string Format(Person person)
+        {
+            return $"{person.Name}, {person.Age} years old.".ToUpper();
+        }
+    }
+}
---
+++ Sammak.SandBox.Ioc/Startup.cs
@@ -0,0 +1,96 @@
+﻿using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sammak.SandBox.Ioc
+{
+    /// <summary>
+    /// Structuremap IOC Container Setup - PersonSync APP
+    /// </summary>
+    public partial class Startup
+    {
+        /// <summary>
+        /// Gets or builds the container
+        /// </summary>
+        /// <returns></returns>
+        //public static IContainer EnsureIocSetUp()
+        //{
+        //    if (DependencyResolver.Container == null && !_initializing)
+        //        return UseIoc();
+        //    return DependencyResolver.Container;
+        //}
+
+        private static bool _initializing;
+
+        /// <summary>
+        /// Gets or sets the container (private).
+        /// </summary>
+        /// <value>
+        /// The container.
+        /// </value>
+        public static IContainer Container { get; set; }
+
+        /// <summary>
+        /// Builds the container (private).
+        /// </summary>
+        /// <returns></returns>
+        protected internal static IContainer UseIoc()
+        {
+            //Logger.Trace("UseIoc: Initializing Container.");
+            _initializing = true;
+
+            try
+            {
+                Container = new Container(c =>
+                {
+                    //c.For<System.Configuration.Abstractions.IConfigurationManager>()
+                    //    .Use(() => ConfigurationManager);
+
+                    c.Scan(s =>
+                    {
+                        s.TheCallingAssembly();
+                        s.AssembliesFromApplicationBaseDirectory();
+                        s.IncludeNamespace("Sammak.SandBox.Ioc");
+                        s.LookForRegistries();
+                        s.WithDefaultConventions();
+                    });
+
+                    //c.For<ISessionFactory>().Singleton().Use(ConfigureOrm());
+                    //c.For<ISession>().LifecycleIs(new ThreadLocalStorageLifecycle()).Use(ctx => ctx.GetInstance<ISessionFactory>().OpenSession());
+                    //c.For<IBackgroundJobClient>().Use<BackgroundJobClient>();
+                    //c.For<IValidationFactory>().Use<ValidationFactory>().Ctor<IContainer>("container").Is(DependencyResolver.Container);
+                    //c.For<IQueryFactory>().Use<QueryFactory>().Ctor<IContainer>("container").Is(DependencyResolver.Container);
+                    //c.For<Core.MassTransit.IBusFactory>().Use<Core.MassTransit.BusFactory>();
+                    //c.For<IEnumService>().Singleton().Use<EnumService>();
+                    //c.For<IValidator<AuditData>>().Singleton().Use<AuditData.AuditDataValidator>();
+                    //c.For<IAccessTokenService>().Singleton().Use<AccessTokenService>();
+                    //c.For<IRegistrationInterservice>().Singleton().Use<RegistrationInterservice>();
+                    //c.For<IProfileInterservice>().Singleton().Use<ProfileInterservice>();
+                    //c.For<IProgramInterservice>().Singleton().Use<ProgramInterservice>();
+                    //c.For<IProductInterservice>().Singleton().Use<ProductInterservice>();
+                    //c.For<ICacheManager<ExamFileData>>().Singleton().Use(d => CacheFactory.FromConfiguration<ExamFileData>("cache", UseServiceCacheManagement()));
+
+                    //FluentValidation.AssemblyScanner.FindValidatorsInAssemblyContaining<ExamFileData.ExamFileDataValidator>()
+                    //    .ForEach(r =>
+                    //    {
+                    //        c.For(r.InterfaceType)
+                    //            .Singleton()
+                    //            .Use(r.ValidatorType);
+                    //    });
+                });
+                //DependencyResolver.Container = Container;
+            }
+            finally
+            {
+                _initializing = false;
+            }
+
+            //Logger.Trace("UseIoc: Container initialized.");
+            return Container;
+            //return DependencyResolver.Container;
+        }
+    }
+}
